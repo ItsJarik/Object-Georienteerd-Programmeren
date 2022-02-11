530e4e94b86c053a62672064c8e7882380cfe5f8
ItsJarik <jarikokkerman@hotmail.nl>
2022-02-11 11:47:04 +0100

    Commit

--- Week 1/Breuken.cpp
+++ Week 1/Breuken.cpp
@@ -21,24 +21,28 @@ void Breuk::drukaf()
 
 void Breuk::leesin()
 {
+    cout << "Geef een Teller?" << endl;
     cin >> teller;
+    cout << "Geef een Noemer?" << endl;
     cin >> noemer;
 }
 
 void Breuk::verhoog(Breuk breuk)
 {
-    teller = teller * breuk.noemer + noemer * breuk.teller;
+    teller = (teller * breuk.noemer) + (noemer * breuk.teller);
     noemer = noemer * teller;
 }
 
 int main()
 {
-    Breuk b;
-    b.leesin();
+    Breuk b1;
     Breuk b2;
+
+    b1.leesin();
     b2.leesin();
-    b.verhoog(b2);
-    b.drukaf();
+
+    b1.verhoog(b2);
+    b1.drukaf();
 
     return 0;
 }
