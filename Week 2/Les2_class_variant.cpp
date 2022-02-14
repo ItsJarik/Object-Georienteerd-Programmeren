a1ef328343ef730d82b7518847dfede0d05d573a
ItsJarik <jarikokkerman@hotmail.nl>
2022-02-14 15:27:40 +0100

    Commit

--- Week 2/Les2_class_variant.cpp
+++ Week 2/Les2_class_variant.cpp
@@ -2,46 +2,10 @@
 
 using namespace std;
 
-// Struct gedeelte
-/*
-typedef struct
-{
-    int teller;
-    int noemer;
-} Breuk;
-
-void verdubbel(Breuk *p)
-{
-    if ((p->noemer % 2) == 0)
-    {
-        p->noemer /= 2;
-    }
-    else
-    {
-        p->teller *= 2;
-    }
-}
-
-int main()
-{
-    Breuk b;
-
-    b.teller = 3;
-    b.noemer = 4;
-
-    verdubbel(&b);
-
-    cout << b.teller << "/" << b.noemer << endl;
-
-    return 0;
-}
-*/
-
-// Class Gedeelte
-
 class Breuk
 {
 public:
+    Breuk();
     Breuk(int t, int n);
     void verdubbel();
     void drukaf();
@@ -50,6 +14,10 @@ private:
     int teller;
     int noemer;
 };
+
+Breuk::Breuk()
+{
+}
 
 Breuk::Breuk(int t, int n)
 {
@@ -77,6 +45,7 @@ void Breuk::verdubbel()
 int main()
 {
     Breuk b(3, 4);
+    Breuk c(b); // Copy Constructor
 
     b.verdubbel();
     b.drukaf();
