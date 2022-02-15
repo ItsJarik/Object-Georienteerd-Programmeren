66890251ba079f9e388fc9c08aeadb43d64212ad
ItsJarik <jarikokkerman@hotmail.nl>
2022-02-15 16:20:17 +0100

    Commit

--- Practicum1/Practicum1a.cpp
+++ Practicum1/Practicum1a.cpp
@@ -16,6 +16,24 @@ private:
     int noemer;
 };
 
+int ggd(int n, int m)
+{
+    if (n == 0)
+        return m;
+    if (m == 0)
+        return n;
+    if (n < 0)
+        n = -n;
+    if (m < 0)
+        m = -m;
+    while (m != n)
+        if (n > m)
+            n -= m;
+        else
+            m -= n;
+    return n;
+}
+
 Breuk::Breuk(int t, int n)
 {
     teller = t;
@@ -37,13 +55,23 @@ void Breuk::verhoog(Breuk b1, Breuk b2)
 
 void Breuk::vereenvoudig()
 {
+    if (noemer < 0)
+    {
+        noemer = -noemer;
+        teller = -teller;
+    }
+
+    int d(ggd(teller, noemer));
+
+    teller /= d;
+    noemer /= d;
 }
 
 int main()
 {
     Breuk b1(2, 4);
     Breuk b2(1, 3);
-    Breuk b3(0,0);
+    Breuk b3(0, 0);
 
     b3.verhoog(b1, b2);
 
