e670abc29275779c91ce571f2fa3438c4ea48037
ItsJarik <jarikokkerman@hotmail.nl>
2022-03-07 16:29:51 +0100

    Commit

--- Practicum 4/InfoLes4.cpp
+++ Practicum 4/InfoLes4.cpp
@@ -9,12 +9,36 @@ void wissel(int &a, int &b)
     b = c;
 }
 
+int &max(int &a, int &b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int kleinste(int &a, int &b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
 int main()
 {
     int x = 7;
-    int y = 10;
+    int y = 2;
+    int z;
+    int *p = &x;
 
-    wissel(x, y);
+    // wissel(x, y);
+
+    *p = kleinste(x, y);
 
     cout << x << " " << y << endl;
 
