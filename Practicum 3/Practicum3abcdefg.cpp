92c4e35917b980ee211770a370b11610ac103a48
ItsJarik <jarikokkerman@hotmail.nl>
2022-03-10 15:24:03 +0100

    Commit

--- Practicum 3/Practicum3abcdefg.cpp
+++ Practicum 3/Practicum3abcdefg.cpp
@@ -2,21 +2,58 @@
 
 using namespace std;
 
-// Nog niet klaar
-
 class Length
 {
 public:
+    Length();
+    Length(int c);
     Length(int f, int i);
-    Length(int i);
     void drukaf_imperial();
     void drukaf_metric();
-    void drukaf_distance(Length d);
+    int smallest(int centi);
+    float centimeters();
+    void maak_gelijk(Length L);
+    Length largest(Length L)
+    {
+        Length newLength;
+
+        if ((L.feet > feet) && (L.inches > inches))
+        {
+            newLength.feet = L.feet;
+            newLength.inches = L.inches;
+        }
+        else
+        {
+            newLength.feet = feet;
+            newLength.inches = inches;
+        }
+
+        return newLength;
+    }
+
+    Length operator+(Length const &L)
+    {
+        Length newLength;
+        newLength.feet = feet + L.feet;
+        newLength.inches = inches + L.inches;
+        return newLength;
+    }
 
 private:
     int feet;
     int inches;
+    int centimeter;
 };
+
+Length::Length()
+{
+
+} // Default Constructor
+
+Length::Length(int c)
+{
+    centimeter = c;
+}
 
 Length::Length(int f, int i)
 {
@@ -24,11 +61,45 @@ Length::Length(int f, int i)
     inches = i;
 }
 
-Length::Length(int i)
+int Length::smallest(int centi)
 {
-    feet = i / 12;
-    inches = i % 12;
+    if (centimeter < centi)
+    {
+        return centimeter;
+    }
+    else
+    {
+        return centi;
+    }
 }
+
+float Length::centimeters()
+{
+    float lokaal_meter;
+    float lokaal_centimeter;
+    float centimeter;
+
+    lokaal_meter = feet * 0.304800;
+    lokaal_centimeter = inches * 2.54;
+
+    centimeter = lokaal_centimeter + (lokaal_meter * 100);
+
+    return centimeter;
+}
+
+void Length::maak_gelijk(Length L)
+{
+    if (feet > L.feet)
+    {
+        feet = L.feet;
+        inches = L.inches;
+    }
+    else
+    {
+        L.feet = feet;
+        L.inches = inches;
+    }
+} // De waardes van de grootste lengte worden verandert naar de waardes van de kleinste lengte
 
 void Length::drukaf_imperial()
 {
@@ -38,30 +109,82 @@ void Length::drukaf_imperial()
 
 void Length::drukaf_metric()
 {
-    float meter = 0.304800;
-    float centimeter = 2.54;
-
-    cout << "Meter: " << feet * meter << endl;
-    cout << "Centimeter: " << inches * centimeter << endl;
+    cout << "Meter: " << feet * 0.304800 << endl;
+    cout << "Centimeter: " << inches * 2.54 << endl;
 }
 
-void Length::drukaf_distance(Length d)
+class Voorwerp
 {
-    int feet_lokaal = 0;
-    int inches_lokaal = 0;
+public:
+    Voorwerp(Length *a);
 
-    feet_lokaal = d.feet - feet;
-    inches_lokaal = d.inches - inches;
+private:
+    Length *lengte;
+    int nummer;
+};
 
-    cout << "Verschil in afstand is: " << endl;
-    cout << "Feet: " << feet_lokaal << endl;
-    cout << "Inches: " << inches_lokaal << endl;
+Voorwerp::Voorwerp(Length *a)
+{
+    lengte = a;
+    nummer = -1;
 }
 
 int main()
 {
+    // Opdracht 1
+
+    /*
     Length d1(1, 2);
     Length d2(2, 4);
+    Length d3;
 
-    d1.drukaf_distance(d2);
+    d3 = d1.largest(d2);
+    */
+
+    // Opdracht 2
+
+    /*
+    Length C(10);
+    int centimeter;
+
+    centimeter = C.smallest(5);
+
+    cout << centimeter << endl;
+    */
+
+    // Opdracht 3
+
+    /*
+    float centimeter;
+
+    centimeter = d1.centimeters();
+    cout << centimeter << endl;
+    */
+
+    // Opdracht 4
+
+    /*
+    Length length1(8, 6);
+    Length length2(2, 4);
+
+    length1.maak_gelijk(length2);
+    length1.drukaf_imperial();
+    length2.drukaf_imperial();
+    */
+
+    // Opdracht 5
+
+    /*
+    Length l1(1, 4);
+    Length l2(2, 6);
+    Length l3 = l1 + l2;
+
+    l3.drukaf_imperial();
+    */
+
+    // Opdracht 6 & 7
+
+    /*
+    Voorwerp V(&d2);
+    */
 }
