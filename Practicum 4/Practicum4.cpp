552b3029381cfeab022f03a7ac9dce31c388fa56
ItsJarik <jarikokkerman@hotmail.nl>
2022-03-15 16:22:59 +0100

    Commit

---
+++ Practicum 4/Practicum4.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+using namespace std;
+
+class Rechthoek
+{
+public:
+    Rechthoek();
+    Rechthoek(int l, int b);
+
+    Rechthoek omvattend(Rechthoek R)
+    {
+        Rechthoek newR;
+
+        newR.lengte = lengte + R.lengte;
+        newR.breedte = breedte + R.breedte;
+
+        return newR;
+    }
+
+    int Viervoud();
+    int Oppervlakte();
+    void showValue();
+
+private:
+    int lengte;
+    int breedte;
+};
+
+Rechthoek::Rechthoek()
+{
+}
+
+Rechthoek::Rechthoek(int l, int b)
+{
+    lengte = l;
+    breedte = b;
+}
+
+int Rechthoek::Oppervlakte()
+{
+    int oppervlakte;
+
+    // Oppervlkate wordt berekend
+    oppervlakte = lengte * breedte;
+
+    // Er wordt een oppervlakte geretourneerd
+    return oppervlakte;
+}
+
+int Rechthoek::Viervoud()
+{
+    int oppervlakte;
+
+    // Verdubbeld de lengte en de breedte
+    lengte = lengte * 2;
+    breedte = breedte * 2;
+
+    // Oppervlakte wordt berekend met de verdubbelde waardes
+    oppervlakte = lengte * breedte;
+
+    // Er wordt een oppervlakte geretouneerd die 4x zo groot is
+    return oppervlakte;
+}
+
+void Rechthoek::showValue()
+{
+    cout << "Lengte: " << lengte << endl;
+    cout << "Breedte: " << breedte << endl;
+}
+
+int main()
+{
+    // Opdracht 1
+
+    /*
+    // Wordt een object van de klasse vierkant aangemaakt
+    Rechthoek R(2, 4);
+
+    // Print alle waarde van de private attributes uit
+    R.showValue();
+    */
+
+    // Opdracht 2
+
+    /*
+    Rechthoek R1(5, 6);
+
+    int oppervlakte = R1.Oppervlakte();
+
+    cout << "Oppervlakte: " << oppervlakte << endl;
+    */
+
+    // Opdracht 3
+
+    /*
+    Rechthoek R2(4, 3);
+
+    int oppervlakte = R2.Oppervlakte();
+    int oppervlakteViervoud = R2.Viervoud();
+
+    cout << "Oppervlakte: " << oppervlakte << endl;
+    cout << "Oppervlakte Verviervoudigd: " << oppervlakteViervoud << endl;
+    */
+
+    // Opdracht 4
+
+    /*
+    Rechthoek R3(6, 3);
+    Rechthoek R4(6, 3);
+    Rechthoek R5;
+
+    R5 = R4.omvattend(R3);
+
+    R5.showValue();
+    */
+
+    return 0;
+}
