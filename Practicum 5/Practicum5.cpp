a14dbe10a188b0a51cd474236006cc4b85179628
ItsJarik <jarikokkerman@hotmail.nl>
2022-03-17 13:52:28 +0100

    Commit

---
+++ Practicum 5/Practicum5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Vorm
+{
+public:
+    int Oppervlakte();
+private:
+protected:
+};
+
+class Vierkant : public Vorm
+{
+public:
+private:
+protected:
+};
+
+class Rechthoek : public Vorm
+{
+public:
+private:
+protected:
+};
+
+int Vorm::Oppervlakte() {
+    return 0;
+}
+
+int main()
+{
+
+    return 0;
+}
